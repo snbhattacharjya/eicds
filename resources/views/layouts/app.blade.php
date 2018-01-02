2e7e5db8e8fb75815edc95db42012059bb8d29c2
Saikat Narayan Bhattacharjya <snbhattacharjya@gmail.com>
2018-01-02 08:02:15 +0530

    Fav Icon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
+    <link rel='shortcut icon' type='image/x-icon' href='/favicon.ico' />
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Styles -->
